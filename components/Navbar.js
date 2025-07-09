a06eabdf91fb732a3d478ff96e44053cc3c3ed8f
Dues-ExMachina <anubhabd45@gmail.com>
2025-07-09 23:41:05 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <Link href="/Contact"><li className="li">Contact</li ></Link>
                 <li className="flex gap-3">
                     <Link href="/Shorten"><button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Try Now</button></Link>
-                    <Link href="/github"><button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">GitHub</button></Link>
+                    <Link href="https://github.com/Dues-ExMachina/TinyLink_URL-shortner"><button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">GitHub</button></Link>
                 </li>
             </ul>
         </nav>
